7372877328226e5a7e8f62f721dc723d7d4b5521
olddevai <pompom.kah@gmail.com>
2024-12-15 18:13:48 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import { Suspense } from 'react';
-import Hero from './components/sections/Hero';
-import About from './components/About';
-import Skills from './components/sections/Skills';
-import Projects from './components/sections/Projects';
-import Contact from './components/Contact';
+import React, { Suspense, lazy } from 'react';
+
+// Lazy load components
+const Hero = lazy(() => import('./components/sections/Hero'));
+const About = lazy(() => import('./components/About'));
+const Skills = lazy(() => import('./components/sections/Skills'));
+const Projects = lazy(() => import('./components/sections/Projects'));
+const Contact = lazy(() => import('./components/Contact'));
 
 function App() {
   return (
     <div className="bg-black text-white">
-      <Suspense fallback={
-        <div className="h-screen flex items-center justify-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
-        </div>
-      }>
+      <Suspense
+        fallback={
+          <div className="h-screen flex items-center justify-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+          </div>
+        }
+      >
         <Hero />
         <About />
         <Skills />
